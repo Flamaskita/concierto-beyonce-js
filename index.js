0f20d30a23538e4263266f4e37d7a56b963f8d3d
Flamaskita <104011440+Flamaskita@users.noreply.github.com>
2022-05-04 20:58:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Tu código va aquí: 
+let edad=16;
+
+if (edad > 17){
+
+    console.log("Puede entrar.")
+
+    } else if (edad < 14){
+    console.log("Lo siento, eres demasiado joven, vuelve a intentarlo en unos años.No
+    puede entrar.")
+    
+    } else {
+    console.log("Pueden entrar, pero sólo acompañadas de una persona mayor de 18
+    años.")
+    }
